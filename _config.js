f27b58240d60a3d0fda3465e1930a2c3d10e52d0
Kim Sumin <66582899+sumpson0-0@users.noreply.github.com>
2021-02-06 22:59:34 +0900

    Add Google Search Console html

--- _config.js
+++ _config.js
@@ -1,8 +1,5 @@
 module.exports = {
   /** Site MetaData (Required all)*/
-  title: `Title`,                           // (* Required)
-  description: `Blog Description`,          // (* Required)
-  author: `Author`,                         // (* Required)
   language: 'ko-KR',                        // (* Required) html lang, ex. 'en' | 'en-US' | 'ko' | 'ko-KR' | ...
   siteUrl: 'https://',                      // (* Required)
     // ex.'https://junhobaik.github.io'
